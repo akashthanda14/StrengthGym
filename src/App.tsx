cb4562c9a1400d560f0f6102ce6776a5dcd47642
work <akashthanda14@gmail.com>
2025-06-18 14:51:43 +0530

    slideshow integrted

--- src/App.tsx
+++ src/App.tsx
@@ -14,21 +14,80 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import AdminDashboard from './components/AdminDashboard';
 import ClientDashboard from './components/ClientDashboard';
+import { Carousel } from './components/ui/carousel';
+
+type SlideData = {
+  title: string;
+  button: string;
+  src: string;
+};
 
 function Home() {
+
+const slides: SlideData[] = [
+  {
+    title: "Client Transformation 1",
+    button: "View More",
+    src: "https://res.cloudinary.com/dmt4dj8ft/image/upload/v1748902624/IMG_1130_okrpa4.jpg"
+  },
+  {
+    title: "Client Transformation 2",
+    button: "View More",
+    src: "https://res.cloudinary.com/dmt4dj8ft/image/upload/v1748902623/0250e939-6894-4359-ab2e-54f03827ee02_myvsmw.jpg"
+  },
+  {
+    title: "Client Transformation 3",
+    button: "View More",
+    src: "https://res.cloudinary.com/dmt4dj8ft/image/upload/v1748902623/2e7c345e-b97a-4c29-9806-00ea624a5daf_2_tkcvti.jpg"
+  },
+  {
+    title: "Client Transformation 4",
+    button: "View More",
+    src: "https://res.cloudinary.com/dmt4dj8ft/image/upload/v1748902621/983ecf4e-efa3-4231-8886-2aac7e619b98_yfv42q.jpg"
+  },
+  {
+    title: "Client Transformation 5",
+    button: "View More",
+    src: "https://res.cloudinary.com/dmt4dj8ft/image/upload/v1748902622/8033ccbe-c9e0-47e6-b2af-89699b34a67e_tqskc8.jpg"
+  },
+  {
+    title: "Client Transformation 6",
+    button: "View More",
+    src: "https://res.cloudinary.com/dmt4dj8ft/image/upload/v1748902621/5fc74786-a26c-4dfb-9b32-9c1e769ce484_ihl2fs.jpg"
+  },
+  {
+    title: "Client Transformation 7",
+    button: "View More",
+    src: "https://res.cloudinary.com/dmt4dj8ft/image/upload/v1748902621/80b83e26-3edc-47c1-9366-9d43ad6a8e71_i61yej.jpg"
+  },
+  {
+    title: "Client Transformation 8",
+    button: "View More",
+    src: "https://res.cloudinary.com/dmt4dj8ft/image/upload/v1748902620/f6cc1f43-2430-4c07-8216-0922e043d559_jouenq.jpg"
+  },
+  {
+    title: "Client Transformation 9",
+    button: "View More",
+    src: "https://res.cloudinary.com/dmt4dj8ft/image/upload/v1748902620/dd9ecb1e-759f-48ae-86b2-2733ebb0b62e_c9wi8f.jpg"
+  }
+];
+
+
+
   return (
     <>
       <Navbar />
       <main>
         <Hero />
-        <ClientSlideShow />
+         <Carousel slides={slides} />
         <About />
         <AppleCarousel />
         <Facilities />
         <Pricing />
         <Contact />
         <SocialFeeds />
-      </main>
+      
+       </main>
       <Footer />
     </>
   );
